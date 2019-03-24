0000e6893c6ca93c866bd56bbaaed02260e9016e
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-03-24 17:07:51 +0100

    Initial project setup, defined a couple of views, controllers.

---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+
+        public ViewResult RecieveForm(string name, string city)
+        {
+            //var name = Request.Form["name"];
+            //var city = Request.Form["city"];
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
