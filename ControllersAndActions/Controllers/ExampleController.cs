d612c8e9c3c84d662c67ee6a9b898ac727dc45e4
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-03-24 18:06:35 +0100

    Finished implementation of chapter.

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController: Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        public ViewResult Result() => View((object)"Hello World");
+
+        public RedirectToActionResult Redirect() => RedirectToAction("Index");
+        //  public RedirectToActionResult Redirect() => RedirectToAction("Index", "Home");
+        //public RedirectToRouteResult Redirect() => RedirectToRoute(new { controller = "Example", action = "Index", ID = "MyID" });
+        //public RedirectResult Redirect() => RedirectPermanent("Example/Index"); //Redirect("Example/Index");
+    }
+}
--- ControllersAndActions/Controllers/HomeController.cs
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -1,19 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControllersAndActions.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ControllersAndActions.Controllers
 {
     public class HomeController: Controller
     {
-        public ViewResult Index() => View("SimpleForm");
+        //public JsonResult Index() => Json(new[] { "Alice", "Bob", "Joe" });
 
-        public ViewResult RecieveForm(string name, string city)
+        //public ObjectResult Index() => Ok(new string[] { "Alice", "Bob", "Joe" });
+
+        //public VirtualFileResult Index() => File("lib/bootstrap/dis/css/bootstrap.css", "text/css");
+
+        //public StatusCodeResult Index() => StatusCode(StatusCodes.Status404NotFound);
+
+        public StatusCodeResult Index() => NotFound();
+
+        //public IActionResult RecieveForm(string name, string city) => new CustomHtmlResult
+        //{
+        //    Content = $"{name} lives in {city}"
+        //};
+
+        //render a specific view 
+        //return View("/Views/Admin/Index");
+        [HttpPost]
+        public RedirectToActionResult RecieveForm(string name, string city)
         {
-            //var name = Request.Form["name"];
-            //var city = Request.Form["city"];
+            TempData["name"] = name;
+            TempData["city"] = city;
+            return RedirectToAction(nameof(Data));
+        }
+
+        public ViewResult Data()
+        {
+            string name = TempData["name"] as string;
+            string city = TempData["city"] as string;
             return View("Result", $"{name} lives in {city}");
         }
     }
